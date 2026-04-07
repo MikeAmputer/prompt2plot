de8767f0b1ae57c20e2000128e55487419085bd3
Pavel Kravtsov <kravtsovpo@gmail.com>
2026-04-08 00:52:04 +0700

    *

--- src/Prompt2Plot/PromptPipeline/PromptContext.cs
+++ src/Prompt2Plot/PromptPipeline/PromptContext.cs
@@ -4,5 +4,5 @@ public sealed class PromptContext
 {
 	public required string NaturalLanguageRequest { get; init; }
 	public string Prompt { get; set; } = string.Empty;
-	public List<string> Errors { get; set; } = [];
+	public List<string> Errors { get; } = [];
 }
